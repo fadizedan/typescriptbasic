c524034eb4f1fbe936ae3342247eb27c2aeb8692
Reem Saleem-Zedan <fadizedan@gmail.com>
2023-06-04 21:13:21 +0300

    first commit

---
+++ build/main.js
@@ -0,0 +1,17 @@
+var a;
+a = 200;
+var b;
+b = 200;
+b = "true";
+b = new Date();
+var c;
+c = true;
+c = "hello";
+c = 145;
+var f;
+f = "hello";
+f = 250;
+function func(age, name) {
+    console.log(name + "is" + age + "years old");
+}
+func(34, "Erez");
---
+++ src/main.ts
@@ -0,0 +1,25 @@
+
+let a: number;
+
+a = 200;
+
+let b: number | string | Date;
+b = 200;
+b = "true";
+b = new Date();
+
+let c: any;
+c=true;
+c = "hello";
+c = 145;
+
+let f;
+f = "hello";
+f = 250;
+
+function func( age: number, name: string){
+    console.log(name + "is " + age + "years old");
+
+}
+
+func(34, "Erez");
